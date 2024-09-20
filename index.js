c339c7f19a0d512934d639779081efa5bd50a71b
Jonatas Cruz <131266716+JntCruz99@users.noreply.github.com>
2024-09-20 19:06:38 -0300

    att

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 
-app.listen(3000, () => console.log('Server executando...'));
+app.listen(80, () => console.log('Server executando...'));
 
 app.get('/skill',(req, res) => {
     res.send({
